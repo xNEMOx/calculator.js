4250949fa260e600d7817c97ea3dfec68d7fad4f
Nemo <63228128+xNEMOx@users.noreply.github.com>
2022-04-24 11:59:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+    console.log(parseInt(num1) + parseInt(num2));
+}
+
+function minus(num1, num2) {
+    console.log(parseInt(num1) - parseInt(num2));
+}
+
+function multiply(num1, num2) {
+    console.log(parseInt(num1) * parseInt(num2));
+}
+
+function divide(num1, num2) {
+    console.log(parseInt(num1) / parseInt(num2));
+}
+
+
+const num1 = prompt("first number: ");
+const symbol = prompt("symbol: ");
+const num2 = prompt("second number: ");
+
+
+if (symbol == '+') {
+    add(num1, num2)
+}
+
+if (symbol == '-') {
+    minus(num1, num2)
+}
+
+if (symbol == '*') {
+    multiply(num1, num2)
+}
+
+if (symbol == '/') {
+    divide(num1, num2)
+}
